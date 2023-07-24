f909a9bf6197687c00ecdbb421bbaf5b1a7067f7
--replace-all <ninganme0317@gmail.com>
2023-07-24 10:35:52 +0800

    NEW: ants registration add param: image type for volume or time series

--- registration/ants_apply_transforms.py
+++ registration/ants_apply_transforms.py
@@ -3,17 +3,18 @@ import ants
 """reference
 https://antspy.readthedocs.io/en/latest/registration.html
 
-python ants_apply_transforms.py --moving_image moving_image.nii.gz --fixed_image fixed_image.nii.gz --output_image output_image.nii.gz --input_transform transform.mat transform1.mat 
+python ants_apply_transforms.py --moving-image moving_image.nii.gz --fixed-image fixed_image.nii.gz --output-image output_image.nii.gz --input-transform transform.mat transform1.mat 
 """
 
 # 创建一个命令行解析器
 parser = argparse.ArgumentParser(description='ANTS registration script')
 
 # 添加需要的命令行参数
-parser.add_argument('--moving_image', type=str, help='path to moving image')
-parser.add_argument('--fixed_image', type=str, help='path to fixed image')
-parser.add_argument('--output_image', type=str, help='path to output image')
-parser.add_argument('--input_transform', type=str, nargs='+', default=['transform.mat'],
+parser.add_argument('--moving-image', type=str, help='path to moving image')
+parser.add_argument('--fixed-image', type=str, help='path to fixed image')
+parser.add_argument('--output-image', type=str, help='path to output image')
+parser.add_argument('--imagetype', type=int, default=0, help='choose 0/1/2/3 mapping to scalar/vector/tensor/time-series')
+parser.add_argument('--input-transform', type=str, nargs='+', default=['transform.mat'],
                     help='path to input transform files')
 
 # 解析命令行参数
@@ -25,7 +26,7 @@ moving_image = ants.image_read(args.moving_image)
 
 # 将变换应用到移动图像上
 warped_image = ants.apply_transforms(fixed=fixed_image, moving=moving_image,
-                                     transformlist=args.input_transform)
+                                     transformlist=args.input_transform, imagetype=args.imagetype)
 
 # 将结果保存为 NIfTI 格式文件
 ants.image_write(warped_image, args.output_image)
--- registration/ants_registration.py
+++ registration/ants_registration.py
@@ -1,22 +1,22 @@
-import sh
+import os
 import argparse
 import ants
 """reference
 https://antspy.readthedocs.io/en/latest/registration.html
 
-python ants_registration.py --moving_image moving_image.nii.gz --fixed_image fixed_image.nii.gz --output_image output_image.nii.gz --transform SyN  --output_transform transform.mat
+python ants_registration.py --moving-image moving_image.nii.gz --fixed-image fixed_image.nii.gz --output-image output_image.nii.gz --transform SyN  --output-transform transform.mat
 """
 
 # 创建一个命令行解析器
 parser = argparse.ArgumentParser(description='ANTS registration script')
 
 # 添加需要的命令行参数
-parser.add_argument('--moving_image', type=str, help='path to moving image')
-parser.add_argument('--fixed_image', type=str, help='path to fixed image')
-parser.add_argument('--output_image', type=str, help='path to output image')
+parser.add_argument('--moving-image', type=str, help='path to moving image')
+parser.add_argument('--fixed-image', type=str, help='path to fixed image')
+parser.add_argument('--output-image', type=str, help='path to output image')
 parser.add_argument('--transform', type=str, default='SyN',
                     help='type of transform to use (default: SyN)')
-parser.add_argument('--output_transform', type=str, default='transform.mat',
+parser.add_argument('--output-transform', type=str, default='transform.mat',
                     help='path to output transform file')
 
 # 解析命令行参数
@@ -27,8 +27,7 @@ fixed_image = ants.image_read(args.fixed_image)
 moving_image = ants.image_read(args.moving_image)
 
 # 进行图像配准
-registration = ants.registration(fixed=fixed_image, moving=moving_image,
-                                  type_of_transform=args.transform)
+registration = ants.registration(fixed=fixed_image, moving=moving_image, type_of_transform=args.transform)
 
 # 获取变换矩阵
 transform_matrix = registration['fwdtransforms']
@@ -42,4 +41,4 @@ ants.image_write(warped_image, args.output_image)
 
 # 将变换矩阵保存到文件中
 print(f'>>> cp transform_matrix {transform_matrix} to {args.output_transform}')
-sh.cp(transform_matrix, args.output_transform)
+os.system(f'mv {transform_matrix[0]} {args.output_transform}')
